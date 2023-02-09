780c65b55637df161c61ef6f16e109a38280d27e
NaviXPS <darkbloomvivian61@gmail.com>
2023-02-09 18:17:17 +0800

    added read to csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import random
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -7,6 +8,11 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+# words
+word_dict = pd.read_csv("data/french_words.csv").to_dict(orient="records") # list of dicts
+
+
+# canvas
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
@@ -15,6 +21,7 @@ canvas.create_text(400, 263, text='word', font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 
+# buttons
 cross_image = PhotoImage(file='images/wrong.png')
 unknown_button = Button(image=cross_image)
 unknown_button.grid(row=1, column=0)
